00d3594ce52f5ac973fc9fcb989ce30cd9c15604
DereviankinNikita <derevyankin13@gmail.com>
2020-11-17 14:06:31 +0200

    New draft for JavaPractice framework

---
+++ src/main/java/RomaPractice/lesson1/Numbers.java
@@ -0,0 +1,18 @@
+package RomaPractice.lesson1;
+
+public class Numbers {
+    //Примитивные типы данный
+
+    byte a;         //2^8 = 256                     (от 128 до 127)
+    short b;        //2^16 = 65.538                 (от -32768 до 32767)
+    int c;
+    long d;
+
+    char e;
+    //Целочисленные типы данных (byte, short, int, long);
+
+    float f;
+    double g;
+
+    boolean h;
+}
