66502c500f86ec3d228f009117d465c0c207e7fc
DereviankinNikita <derevyankin13@gmail.com>
2020-11-17 15:40:36 +0200

    Linked data types

---
+++ src/main/java/NikitaPractice/Operation.java
@@ -0,0 +1,5 @@
+package NikitaPractice;
+
+public class Operation {
+
+}
---
+++ src/main/java/NikitaPractice/Reference.java
@@ -0,0 +1,11 @@
+package NikitaPractice;
+
+public class Reference {
+    String str = "My string";                           /** Ссылка на объект */
+    String str2 = new String("My String");      /** Ссылка на объект */
+
+    public void foo(){
+
+    }
+    //https://youtu.be/nM2QF3r_PCM?t=304
+}
