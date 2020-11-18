7961eb163a5f9500730f3041357ec80eb9ab157a
DereviankinNikita <derevyankin13@gmail.com>
2020-11-18 13:05:06 +0200

    Changes

--- src/main/java/NikitaPractice/Reference.java
+++ src/main/java/NikitaPractice/Reference.java
@@ -4,6 +4,10 @@ public class Reference {
     String str = "My string";                           /** Ссылка на объект */
     String str2 = new String("My String");      /** Ссылка на объект */
 
+    /** Разница между примитивным типом данных и сслыочном
+     * Обращайся к ссылке - обращаемся к методам данного объекта
+     *
+     */
     public void foo(){
 
     }
