f5c46bc1c6d329bc12edb33edc27dbae41a2df09
DereviankinNikita <derevyankin13@gmail.com>
2020-11-19 14:38:03 +0200

    refactor

---
+++ src/main/java/RomaPractice/Main.java
@@ -0,0 +1,19 @@
+package RomaPractice;
+
+import RomaPractice.BusLesson7;
+
+public class Main {
+    public static void main(String[] args) {    //psvm
+        BusLesson7 ourBusLesson7 = ourBusLesson7 = new BusLesson7();  //для создания объекта исп. ключевое слово new и конструктор класса (Bus)
+        BusLesson7 firstBusLesson7 = firstBusLesson7 = new BusLesson7(); // firstBus объект типа Bus
+        BusLesson7 secondBusLesson7 = secondBusLesson7 = new BusLesson7();
+
+        ourBusLesson7.color = "Green";
+        firstBusLesson7.color = "Red";
+        secondBusLesson7.color = "Grey";
+
+        ourBusLesson7.showColor();
+        firstBusLesson7.showColor();
+        secondBusLesson7.showColor();
+    }
+}
