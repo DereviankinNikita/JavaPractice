87edfa5420527ac600a8325a38910b2a70412dcf
0dmeral <moscowman01@gmail.com>
2020-11-24 13:25:17 +0300

    Dorabotal nemnogo

--- src/main/java/RomaPractice/MaximVlasenko.java
+++ src/main/java/RomaPractice/MaximVlasenko.java
@@ -3,19 +3,22 @@ package RomaPractice;
 public class MaximVlasenko {
     public static void main(String[] args) {
         Gamer max = new Gamer();
-        max.gameLvl = 5;
+        max.gameLvl = 10;
         max.age = 22;
         max.show();
         int a = 1000, b = 1500, c = 2000;
 
-        if (max.hdcrLvl < a) {
-            System.out.println("Уровень вашего скилла -" + max.hdcrLvl + " единиц\nОчень низкий - git good!");
-        }
-        if (max.hdcrLvl > a && max.hdcrLvl < b) {
-            System.out.println("Ваш уровень скилла - " + max.hdcrLvl + " единиц\nСредний показатель, есть куда расти.");
-        }
-        if (max.hdcrLvl > c) {
-            System.out.println("Уровень вашего скилла - " + max.hdcrLvl + " единиц\nОчень высокий уровень!!!");
-        }
+        if (max.hdcrLvl < a)
+            System.out.println("Уровень вашего скилла = " + max.hdcrLvl + " единиц\nОчень низкий - git good!");
+
+        if (max.hdcrLvl > a && max.hdcrLvl < b)
+            System.out.println("Ваш уровень скилла = " + max.hdcrLvl + " единиц\nСредний показатель, есть куда расти.");
+
+        if (max.hdcrLvl > b && max.hdcrLvl <= c) {
+            System.out.println("Уровень вашего скилла = " + max.hdcrLvl + " единиц\nОчень высокий уровень!!!");
+        } else if (max.hdcrLvl > c)
+            System.out.println("Уровень вашего скилла = " + max.hdcrLvl + "единиц\nПросто божественный уровень!!!");
     }
+
 }
+
