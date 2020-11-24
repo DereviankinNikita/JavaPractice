8b21bbd18063dc0d57fdcee5107b52af62dd9b14
0dmeral <moscowman01@gmail.com>
2020-11-24 14:08:19 +0300

    FinalVersion(maybe)

--- src/main/java/RomaPractice/Gamer.java
+++ src/main/java/RomaPractice/Gamer.java
@@ -3,9 +3,25 @@ package RomaPractice;
 public class Gamer {
     public String name;
     public int gameLvl, age, hdcrLvl;
+    char d=9164, g=2593;
 
     public void show() {
+        System.out.println("Имя игрока " + d +name + "\n");
         hdcrLvl = (age * gameLvl) * 10;
+        int a = 1000, b = 1500, c = 2000;
+
+        if (hdcrLvl < a && hdcrLvl != 0) {
+            System.out.println("Уровень его скилла = " + hdcrLvl + " единиц\nОчень низкий - git good!");
+        } else if (hdcrLvl == 0)
+            System.out.println("Уровень скилла ЭТОГО РАКА  = " + hdcrLvl + " единиц\nНу ты и рачина, удаляй кампутер");
+        if (hdcrLvl > a && hdcrLvl < b)
+            System.out.println("Его уровень скилла = " + hdcrLvl + " единиц\nСредний показатель, есть куда расти.");
+
+        if (hdcrLvl > b && hdcrLvl <= c) {
+            System.out.println("Уровень его скилла = " + hdcrLvl + " единиц\nОчень высокий уровень!!!");
+        } else if (hdcrLvl > c)
+            System.out.println("Уровень его скилла = " + hdcrLvl + " единиц\nПросто божественный уровень!!!");
+
     }
 }
 
