73a1ecc38d72081862e16609c7416aaa0f979ba3
Tw1stTheBones <molokomax93@gmail.com>
2020-11-24 13:40:22 +0200

    Error

--- src/main/java/MaximPractice/Main.java
+++ src/main/java/MaximPractice/Main.java
@@ -1,12 +1,7 @@
 package MaximPractice;
 
 public class Main {
-    public static void main(String[] args){
-<<<<<<< HEAD
-        System.out.println("sasa");
-=======
-        System.out.println("Я максим и я дурак! Встал позже всех!");
->>>>>>> 18adc252b99057126b7f0adf59f25bd64d2b1814
 
-    }
+        System.out.println("sasa");
+        System.out.println("Я максим и я дурак! Встал позже всех!");
 }
