7d796f6fc77a4138f794a32648ae07c9962969ca
0dmeral <moscowman01@gmail.com>
2020-11-24 14:27:43 +0300

    Added more GAYmers

--- src/main/java/RomaPractice/MaximVlasenko.java
+++ src/main/java/RomaPractice/MaximVlasenko.java
@@ -4,9 +4,21 @@ public class MaximVlasenko {
     public static void main(String[] args) {
         Gamer max = new Gamer();
         max.name = "Максим aka TwistTheBones Власенко";
-        max.gameLvl = 0;
+        max.gameLvl = 10;
         max.age = 22;
         max.show();
+
+        Gamer maxLosev = new Gamer();
+        maxLosev.name="Максим aka Radios Лосев";
+        maxLosev.gameLvl=5;
+        maxLosev.age=22;
+        maxLosev.show();
+
+        Gamer niko= new Gamer();
+        niko.name="Никита aka HRцк Деревянкин";
+        niko.age=21;
+        niko.gameLvl=3;
+        niko.show();
 
     }
 
