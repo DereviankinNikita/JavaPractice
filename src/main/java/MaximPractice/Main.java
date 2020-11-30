d149d437f80df16cf5f8f899b9dd038d6649824d
Tw1stTheBones <molokomax93@gmail.com>
2020-11-30 13:33:45 +0200

    maximTest

--- src/main/java/MaximPractice/Main.java
+++ src/main/java/MaximPractice/Main.java
@@ -2,8 +2,9 @@ package MaximPractice;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("sasa");
-        System.out.println("Я максим и я дурак! Встал позже всех!");
-
+        Car car = new Car();
+        car.start();
+        car.stop();
+        car.drive();
     }
 }
